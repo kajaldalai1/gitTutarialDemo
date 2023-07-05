e5a5934bbcbbbf22d431b8333992dc1a87b39848
Kajal Dalai <kajaldalai1@gmail.com>
2023-07-05 20:59:12 +0530

    new adding of dom

--- DOM/dom.js
+++ DOM/dom.js
@@ -55,16 +55,35 @@
 
 //QUERYSELECTOR//
 
-var header = document.querySelector('#main-header');
-header.style.borderbottom = 'solid 4px #ccc';
-var input = document.querySelector('input');
-input.value = 'hello world';
-var submit = document.querySelector('input[type="submit"]')
-submit.value = "SEND"
-var item = document.querySelector('.list-group-item');
-item.style.color = 'red';
-var lastItem = document.querySelector('.list-group-item:last-child');
-lastItem.style.color = 'blue';
+// var header = document.querySelector('#main-header');
+// header.style.borderbottom = 'solid 4px #ccc';
+// var input = document.querySelector('input');
+// input.value = 'hello world';
+// var submit = document.querySelector('input[type="submit"]')
+// submit.value = "SEND"
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
 
-var  secondItem= document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.color = 'coral';
+// var  secondItem= document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color = 'coral';
+
+// QUERYSELECTORALL //
+// var titles = document.querySelectorAll('.title');
+
+// console.log(titles);
+// titles[0].textContent = 'Hello';
+
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even = document.querySelectorAll('li:nth-child(even)');
+// for(var i=0; i<odd.length;i++){
+//     odd[i].style.backgroundColor = 'green';
+//     even[i].style.backgroundColor = '#ccc';
+// }
+
+var  second= document.querySelectorAll('li:nth-child(second)');
+secondItem.style.backgroundColor = 'green';
+
+var  third= document.querySelectorAll('li:nth-child(second)');
+third.style.backgroundColor = '#f4f4f4';
